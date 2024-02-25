01252738263ec5a2e1f4bf274a61446215096de8
20 <64438648@qq.com>
2024-02-26 00:44:02 +0800

    Delete dswz/app directory

--- dswz/app/files/dswzcommon.js
+++
@@ -1,8 +0,0 @@
-﻿
-var _hmt = _hmt || [];
-(function() {
-    var hm = document.createElement("script");
-    hm.src = "https://hm.baidu.com/hm.js?2b8009d2f2574f990048f5fa633db9ad";
-    var s = document.getElementsByTagName("script")[0]; 
-    s.parentNode.insertBefore(hm, s);
-})();
